0cbe0ab205f6e80b03bd13201c175f3a1d86acf5
Rick <baban567899@gmail.com>
2025-08-14 22:20:56 +0530

    Time: 53 ms (5.22%) | Memory: 98.4 MB (45.57%) - LeetSync

---
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+
+    if (nums.length <= 2){
+        return nums.sort((a,b) => (b - a))
+    }
+
+    let allNeg = nums.filter((elm) => elm < 0)
+    let negC = 0
+    let allPos = nums.filter((elm) => elm > 0)
+    let posC = 0
+    let com = []
+    for(let i = 0; i<= nums.length -1; i++){
+        if ((i+1)%2 == 0){
+            com.push(allNeg[negC])
+            negC++
+        } else {
+            com.push(allPos[posC])
+            posC++
+        }
+    }
+    return com
+};
